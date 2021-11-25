17c3ead9add1819702393e9390f33ed07755afc4
Luca <olivelliluca.97@gmail.com>
2021-11-25 17:15:43 +0100

    created Intellij project for backend whit springboot

---
+++ MountBook_Backend/src/main/java/com/example/mountbook_backend/MountBookBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.mountbook_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MountBookBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MountBookBackendApplication.class, args);
+    }
+
+}
