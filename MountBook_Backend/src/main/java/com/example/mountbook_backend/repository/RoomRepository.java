bbffd348f3fcddfb8cfd4a101e8451e6dda78314
Luca <olivelliluca.97@gmail.com>
2021-12-02 17:16:52 +0100

    added repository and controller for the Room and Shelter

---
+++ MountBook_Backend/src/main/java/com/example/mountbook_backend/repository/RoomRepository.java
@@ -0,0 +1,8 @@
+package com.example.mountbook_backend.repository;
+
+import com.example.mountbook_backend.entity.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository<Room,Long>{
+
+}
---
+++ MountBook_Backend/src/main/java/com/example/mountbook_backend/repository/ShelterRepository.java
@@ -0,0 +1,8 @@
+package com.example.mountbook_backend.repository;
+
+import com.example.mountbook_backend.entity.Shelter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShelterRepository extends JpaRepository<Shelter,Long> {
+
+}
