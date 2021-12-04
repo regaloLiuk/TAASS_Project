578cf3041eb16a5d61eb8e5f80dec7b0afff6d9e
Luca <olivelliluca.97@gmail.com>
2021-12-04 18:48:57 +0100

    added repository and controller for the Room and Shelter

--- MountBook_Backend/src/main/java/com/example/mountbook_backend/controller/HostController.java
+++ MountBook_Backend/src/main/java/com/example/mountbook_backend/controller/HostController.java
@@ -5,14 +5,13 @@ import com.example.mountbook_backend.repository.HostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
+@RestController
+@RequestMapping("/api/v1")
 public class HostController {
 
     @Autowired
