db12d60d037d2a4c04ceeab1bd3f1a50dcca403d
Luca <olivelliluca97@gmail.com>
2022-05-27 19:52:32 +0200

    changed properties e dokerfile to run app w/ Docker and added heroku.yml

--- MountBook_Backend/src/main/resources/data.sql
+++ MountBook_Backend/src/main/resources/data.sql
@@ -21,15 +21,10 @@ INSERT INTO user_roles (user_id, role_id) VALUES (4,1);
 INSERT INTO user_roles (user_id, role_id) VALUES (5,3);
 INSERT INTO user_roles (user_id, role_id) VALUES (5,4);
 
-INSERT INTO shelter (id, name, address, email, web_site, description, max_num_bed, price, altitude, longitude, latitude, open, close, telephone_number) VALUES (1, 'il rifugio della motagna',	'via in cima alla montegna più alta',	'montainrefuge@email.com',		'www.rifugio.it',	'un rifugio montano alla vecchia maniera con tutti i confort moderni',	20, 21.60, 1896, 145.654, -56.9822, '2022-01-01', '2022-05-30', 1123456789);
-INSERT INTO shelter (id, name, address, email, web_site, description, max_num_bed, price, altitude, longitude, latitude, open, close, telephone_number) VALUES (2, 'rifugio Calmamente',		'via poco trafficata',					'rifugiocalmamente@email.com',	'www.rifugio.it',	'un luogo di pace e serenità lontano dallo stress cittadino',			15, 35.80, 2068, -96.214, 65.985, '2022-04-01', '2022-09-30', 1123456789);
-INSERT INTO shelter (id, name, address, email, web_site, description, max_num_bed, price, altitude, longitude, latitude, open, close, telephone_number) VALUES (3, 'lake resort',				'corso intorno al lago',				'lakeshelter@email.com',		'www.rifugio.it',	'un rifugio a due passi dal lago',										40, 40.10, 1569, 176.369, 55.5486, '2022-01-01', '2022-12-31', 1123456789);
+INSERT INTO structure (id, name, address, email, web_site, description, max_num_bed, price, altitude, longitude, latitude, open, close, telephone_number, type) VALUES (1, 'il rifugio della motagna',	'via in cima alla montegna più alta',	'montainrefuge@email.com',		'www.rifugio.it',	'un rifugio montano alla vecchia maniera con tutti i confort moderni',	20, 21.60, 1896, 145.654, -56.9822, '2022-01-01', '2022-05-30', 1123456789,0);
+INSERT INTO structure (id, name, address, email, web_site, description, max_num_bed, price, altitude, longitude, latitude, open, close, telephone_number, type) VALUES (2, 'rifugio Calmamente',		'via poco trafficata',					'rifugiocalmamente@email.com',	'www.rifugio.it',	'un luogo di pace e serenità lontano dallo stress cittadino',			15, 35.80, 2068, -96.214, 65.985, '2022-04-01', '2022-09-30', 1123456789,0);
+INSERT INTO structure (id, name, address, email, web_site, description, max_num_bed, price, altitude, longitude, latitude, open, close, telephone_number, type) VALUES (3, 'lake resort',				'corso intorno al lago',				'lakeshelter@email.com',		'www.rifugio.it',	'un rifugio a due passi dal lago',										40, 40.10, 1569, 176.369, 55.5486, '2022-01-01', '2022-12-31', 1123456789,0);
 
-INSERT INTO bivouac (id, name, bed, is_open, description) VALUES (1, 'bivacco Montenegro', 10, true, 'il caldo riparo del Montenegro a 2040m');
-INSERT INTO bivouac (id, name, bed, is_open, description) VALUES (2, 'bivacco Montebianco', 6, true, 'il caldo riparo del Montebianco a 2300');
-INSERT INTO bivouac (id, name, bed, is_open, description) VALUES (3, 'bivacco Monterosa', 15, false, 'il caldo riparo del Monterosa a 2125m');
-INSERT INTO bivouac (id, name, bed, is_open, description) VALUES (4, 'bivacco Montenonso', 4, true, 'il caldo riparo del Montenonso a 1896m');
-
-INSERT INTO service (id, shelter_id, wifi, equipment, car) VALUES (1,1,true,true, false);
-INSERT INTO service (id, shelter_id, wifi, equipment, car) VALUES (2,2,false ,false , true );
-INSERT INTO service (id, shelter_id, wifi, equipment, car) VALUES (3,3,true,true, true);
+INSERT INTO service (id, structure_id, wifi, equipment, car) VALUES (1,1,true,true, false);
+INSERT INTO service (id, structure_id, wifi, equipment, car) VALUES (2,2,false ,false , true );
+INSERT INTO service (id, structure_id, wifi, equipment, car) VALUES (3,3,true,true, true);
